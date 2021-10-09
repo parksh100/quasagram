da14a20e50e3ae2d7004717589af27970bb94408
shpark <parksh1000@outlook.com>
2021-10-09 15:46:07 +0900

    footer설정완료

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer
+      class="bg-white"
+      bordered
+    >
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab
+          to="/"
+          name="home"
+          icon="eva-home-outline"
+          label="Home"
+        />
+        <q-route-tab
+          to="/camera"
+          name="camera"
+          icon="eva-camera-outline"
+          label="Camera"
+        />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.q-footer
+  .q-tab__icon
+    font-size: 30px
+</style>
